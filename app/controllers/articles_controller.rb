484c9486eb5482c6123177629c909bafb81a0dfb
Rick <rbciti@msn.com>
2016-09-30 13:13:12 -0700

    Refactored article controller and new edit views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   
   def index
   @articles = Article.all   
@@ -25,11 +27,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
 
     respond_to do |format|
       if @article.update(article_params)
@@ -45,11 +45,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
       flash[:success] = "Article has been deleted"
       redirect_to articles_path
@@ -61,5 +59,10 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
+
 
